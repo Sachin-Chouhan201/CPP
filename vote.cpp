4b968df40ca538cfde066103fd4bce62fd61e7ca
Sachin-Chouhan201 <75372486+Sachin-Chouhan201@users.noreply.github.com>
2020-12-08 21:49:16 +0530

    Update vote.cpp

--- vote.cpp
+++ vote.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 using namespace std;
 int main()
-{
+{ 
+    cout<<"\nPROGRAM TO CHECK FOR ELIGINLITY OF VOTEING \n ENTER YOUR AGE ";
     int age;
     cin>>age;
     if(age>=18)
     {
-        cout<<"You Can Vote.";
+        cout<<"You Can Vote."<<endl;
     }
     else
     {
-        cout<<"Not Eligible To Vote.";
+        cout<<"Not Eligible To Vote."<<endl;
     }
     return 0;
+    system("pause");
 }
