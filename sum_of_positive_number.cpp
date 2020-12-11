2b1a842f5134b2db05e184795ad8d3e342d234b1
Sachin-Chouhan201 <75372486+Sachin-Chouhan201@users.noreply.github.com>
2020-12-11 18:12:48 +0530

    Update sum_of_positive_number.cpp

--- sum_of_positive_number.cpp
+++ sum_of_positive_number.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main()
 {
+    system("pause");
+    cout<<"\n PROGRAM TO PRINT SUM OF POSITIVE NUMBER \n";
     int num;
     int sum=0;
     cout<<"Program For SUM Of Positive Number "<<endl;
